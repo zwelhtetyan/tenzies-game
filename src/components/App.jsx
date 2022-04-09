e6fc7ccdad4a58436cf5cff8db94c326fdfbf8f4
Zwel Htet Yan <zwelhtetyan2001@gmail.com>
2022-04-09 16:57:57 +0630

    change dots instead of number

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -67,20 +67,20 @@ const App = () => {
     ));
 
     return (
-        <div>
-            <main>
-                {tenzies && <Confetti />}
-                <h1 className='title'>Tenzies</h1>
-                <p className='instructions'>
-                    {tenzies ? 'You Won 🎉!' : `Roll until all dice are the same. Click each die to freeze
+        <main>
+            {tenzies && <Confetti />}
+            <h1 className='title'>Tenzies</h1>
+            <p className='instructions'>
+                {tenzies
+                    ? 'You Win 🎉'
+                    : `Roll until all dice are the same. Click each die to freeze
                     it at its current value between rolls.`}
-                </p>
-                <div className='container'>{diceElements}</div>
-                <button className='roll' onClick={rollDice}>
-                    {tenzies ? 'New Game' : 'Roll'}
-                </button>
-            </main>
-        </div>
+            </p>
+            <div className='container'>{diceElements}</div>
+            <button className='roll' onClick={rollDice}>
+                {tenzies ? 'New Game' : 'Roll'}
+            </button>
+        </main>
     );
 };
 
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
 
+const showDice = [
+    ['center-dot'],
+    ['topLeft-dot', 'bottomRight-dot'],
+    ['topRight-dot', 'center-dot', 'bottomLeft-dot'],
+    ['topRight-dot', 'topLeft-dot', 'bottomRight-dot', 'bottomLeft-dot'],
+    [
+        'topRight-dot',
+        'topLeft-dot',
+        'center-dot',
+        'bottomRight-dot',
+        'bottomLeft-dot',
+    ],
+    [
+        'topRight-dot',
+        'topLeft-dot',
+        'centerLeft-dot',
+        'centerRight-dot',
+        'bottomRight-dot',
+        'bottomLeft-dot',
+    ],
+];
+
 const Die = (props) => {
+    const generateDot = (value) => {
+        const getDotNameArr = showDice[value - 1]; // index start from zero
+        return getDotNameArr.map((dotName, idx) => (
+            <div className={`dot ${dotName}`} key={idx + 1}></div>
+        ));
+    };
+
     return (
         <div
             value={props.value}
             className={`die ${props.isHeld ? 'held' : ''}`}
             onClick={props.handleHeld}
         >
-            <h2 className='num'>{props.value}</h2>
+            <div className='num'>{generateDot(props.value)}</div>
         </div>
     );
 };
