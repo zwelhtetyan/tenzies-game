d21a264b22c3215bcbd3d394703c231b48de7f32
Zwel Htet Yan <zwelhtetyan2001@gmail.com>
2022-04-11 16:40:51 +0630

    t

--- src/components/MetaTag.jsx
+++ src/components/MetaTag.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { MetaTags } from 'react-meta-tags';
+import preview from '../preview.png';
 
 export default function MetaTag() {
     return (
         <div className='wrapper'>
             <MetaTags>
                 <meta property='og:title' content='Tenzies Game 🎲' />
-                <meta property='og:image' content='../../public/preview.png' />
+                <meta property='og:image' content={preview} />
                 <meta
                     property='og:url'
                     content='https://tenzies-game1.netlify.app'
